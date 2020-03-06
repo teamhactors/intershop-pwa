299e40aebfe21b01b13ec9323d299fedfa9f45af
Nils Kruse <nkruse@intershop.de>
2020-03-06 14:55:10 +0100

    Replaced ngb-carousel with ngx-swiper-wrapper

--- src/app/extensions/wishlists/shared/wishlist-suggestion/wishlist-suggestion.component.ts
+++ src/app/extensions/wishlists/shared/wishlist-suggestion/wishlist-suggestion.component.ts
@@ -1,5 +1,8 @@
-import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject, OnInit } from '@angular/core';
+import { SwiperConfigInterface } from 'ngx-swiper-wrapper';
 import { Observable } from 'rxjs';
+
+import { LARGE_BREAKPOINT_WIDTH } from 'ish-core/configurations/injection-keys';
 
 import { WishlistsFacade } from '../../facades/wishlists.facade';
 import { Wishlist } from '../../models/wishlist/wishlist.model';
@@ -12,8 +15,29 @@ import { Wishlist } from '../../models/wishlist/wishlist.model';
 export class WishlistSuggestionComponent implements OnInit {
   preferredWishlist$: Observable<Wishlist>;
   itemsPerSlide = 4;
+  /**
+   * configuration of swiper carousel
+   * find possible parameters here: http://idangero.us/swiper/api/#parameters
+   */
+  swiperConfig: SwiperConfigInterface;
 
-  constructor(private wishlistsFacade: WishlistsFacade) {}
+  constructor(private wishlistsFacade: WishlistsFacade, @Inject(LARGE_BREAKPOINT_WIDTH) largeBreakpointWidth: number) {
+    this.swiperConfig = {
+      breakpoints: {
+        [largeBreakpointWidth]: {
+          slidesPerView: 2,
+          slidesPerGroup: 2,
+        },
+      },
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+        clickableClass: 'swiper-pagination-clickable',
+      },
+      slidesPerView: 4,
+      slidesPerGroup: 4,
+    };
+  }
 
   ngOnInit() {
     this.preferredWishlist$ = this.wishlistsFacade.preferredWishlist$;
